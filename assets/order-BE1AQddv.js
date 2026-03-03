c5ecdda533050b3df3d6183cbf0bdbda0887e7d0
shiou-yu <a791175@gmail.com>
2026-03-03 01:43:53 +0000

    Deploying to gh-pages from @ shiou-yu/jp@092a401ad0f8636c10e31159e1ad63c85ef90d7c 🚀

--- assets/order-BE1AQddv.js
+++
@@ -1,1 +0,0 @@
-import{aB as e}from"./index-CVfCkyB5.js";const{apiAuth:r}=e(),o={create:()=>r.post("/order"),get(){return r.get("/order")},getAll(){return r.get("/order/all")}};export{o as s};
---
+++ assets/order-D3EYmCu4.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index-aVoDoF-j.js";const{apiAuth:r}=e(),o={create:()=>r.post("/order"),get(){return r.get("/order")},getAll(){return r.get("/order/all")}};export{o as s};
