d2cd841f10dec322af282a541fec0145e7ff43a5
shiou-yu <a791175@gmail.com>
2026-03-03 01:51:52 +0000

    Deploying to gh-pages from @ shiou-yu/jp@c8ebce31f15ef08303310ed0463e10c72b9ec67a 🚀

--- assets/order-BBrnFAwZ.js
+++
@@ -1,1 +0,0 @@
-import{aB as e}from"./index-BEwrpSXU.js";const{apiAuth:r}=e(),o={create:()=>r.post("/order"),get(){return r.get("/order")},getAll(){return r.get("/order/all")}};export{o as s};
---
+++ assets/order-Cyfh6M7S.js
@@ -0,0 +1,1 @@
+import{aB as e}from"./index-CofVLNkp.js";const{apiAuth:r}=e(),o={create:()=>r.post("/order"),get(){return r.get("/order")},getAll(){return r.get("/order/all")}};export{o as s};
